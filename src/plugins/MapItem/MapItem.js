0f7a1645407dd3fff2fba4996a411828c5abcbb4
Henry Liu <thenrikie@hotmail.com>
2018-09-10 10:32:33 +0100

    feat: add map items plugin

---
+++ src/plugins/MapItem/MapItem.js
@@ -0,0 +1,143 @@
+import { Component } from 'react';
+
+const pluginId = 'mapitem';
+const imgCached = {};
+
+const DEFAULT_TEXT_STYLE = {
+  size: '12 px',
+  family: 'Verdana',
+  color: 'black',
+  center: true,
+};
+
+function convertName(name) {
+  if (name.startsWith('ROOM')) {
+    return name.substr(4).trim();
+  }
+
+  return name;
+}
+
+function createImage(src) {
+  if (!imgCached[src]) {
+    const img = new Image();
+    img.src = src;
+    imgCached[src] = img;
+  }
+
+  return imgCached[src];
+}
+
+class MapCanvasPlugin extends Component {
+  shouldComponentUpdate(nextProps) {
+    const { mapItems } = this.props;
+    return mapItems !== nextProps.mapItems;
+  }
+
+  render() {
+    const { mapItems, addMapItems, legends, updateMapItems } = this.props;
+
+    addMapItems(
+      mapItems.reduce((accumulator, { id, coordinates: [x, y], floor, name, type, photo }) => {
+        const baseMapItem = {
+          id: `${floor}_${id}`,
+          floor,
+          x,
+          y,
+        };
+
+        switch (type) {
+          case 'crossBuildingConnector':
+          case 'escalator':
+          case 'restaurant':
+          case 'maleToilet':
+          case 'femaleToilet':
+          case 'stair':
+          case 'expressStation':
+          case 'drinkingFountain':
+          case 'atm':
+          case 'mailbox':
+          case 'taxiStand':
+          case 'virtualBarnWorkstation':
+          case 'satellitePrinter':
+            accumulator.push({
+              ...baseMapItem,
+              image: createImage(legends[type].image),
+            });
+            break;
+          case 'lift':
+            accumulator.push(
+              {
+                ...baseMapItem,
+                id: `${floor}_${id}_circle`,
+                circle: {
+                  radius: 15,
+                  borderColor: '#666666',
+                  color: 'lightblue',
+                },
+              },
+              {
+                ...baseMapItem,
+                id: `${floor}_${id}_text`,
+                textElement: {
+                  ...DEFAULT_TEXT_STYLE,
+                  text: name,
+                },
+              },
+            );
+            break;
+          default:
+            accumulator.push({
+              ...baseMapItem,
+              textElement: {
+                ...DEFAULT_TEXT_STYLE,
+                text: convertName(name),
+              },
+
+              onMouseOver: () => {
+                document.body.style.cursor = 'pointer';
+                updateMapItems([
+                  {
+                    ...baseMapItem,
+                    textElement: {
+                      ...DEFAULT_TEXT_STYLE,
+                      text: convertName(name),
+                      color: 'lightblue',
+                    },
+                  },
+                ]);
+              },
+
+              onMouseOut: () => {
+                document.body.style.cursor = 'auto';
+                updateMapItems([
+                  {
+                    ...baseMapItem,
+                    textElement: {
+                      ...DEFAULT_TEXT_STYLE,
+                      text: convertName(name),
+                    },
+                  },
+                ]);
+              },
+            });
+        }
+
+        if (photo) {
+          accumulator.push({
+            ...baseMapItem,
+            id: `${floor}_${id}_photo`,
+            x: x - 40,
+            image: createImage(legends.photo.image),
+          });
+        }
+
+        return accumulator;
+      }, []),
+    );
+
+    return null;
+  }
+}
+
+export { pluginId, MapCanvasPlugin };
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,5 +1,6 @@
 import * as Example from './Example/Example';
 import * as Example2 from './Example2/Example2';
 import * as MapTile from './MapTile/MapTile';
+import * as MapItem from './MapItem/MapItem';
 
-export default [Example, Example2, MapTile];
+export default [Example, Example2, MapTile, MapItem];
