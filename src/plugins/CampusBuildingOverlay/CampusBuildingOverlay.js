62dc4491dddb4158f46350fe852a2f6aa8cd132b
Henry Liu <thenrikie@hotmail.com>
2018-09-30 20:26:53 +0100

    Add building clickable overlay in campus map

---
+++ src/plugins/CampusBuildingOverlay/CampusBuildingOverlay.js
@@ -0,0 +1,65 @@
+const DEFAULT_COLOR = 'transparent';
+const MOUSEOVER_COLOR = 'rgba(173,216,230,0.7)';
+
+function CampusBuildingOverlay({ floorStore: { floors }, setMapItems, linkTo }) {
+  const getOverlayMapItem = (id, x, y, width, height, floor) => ({
+    id,
+    x,
+    y,
+    floor: 'Overall',
+    rect: {
+      width,
+      height,
+      color: DEFAULT_COLOR,
+    },
+    onMouseOver: () => {
+      document.body.style.cursor = 'pointer';
+      setMapItems([
+        {
+          id,
+          rect: {
+            width,
+            height,
+            color: MOUSEOVER_COLOR,
+          },
+        },
+      ]);
+    },
+    onMouseOut: () => {
+      document.body.style.cursor = 'auto';
+      setMapItems([
+        {
+          id,
+          rect: {
+            width,
+            height,
+            color: DEFAULT_COLOR,
+          },
+        },
+      ]);
+    },
+    onClick: () => {
+      const {
+        [floor]: { defaultScale, defaultX, defaultY },
+      } = floors;
+      linkTo({ floor, x: defaultX, y: defaultY, scale: defaultScale });
+    },
+  });
+
+  setMapItems([
+    getOverlayMapItem('academicBuildingOverlay', 873, 403, 300, 700, 'G'),
+    getOverlayMapItem('CYTOverlay', 970, 1090, 110, 110, 'CYTG'),
+    getOverlayMapItem('LSKOverlay', 1158, 1344, 140, 200, 'NABG'),
+    getOverlayMapItem('IASOverlay', 1408, 1485, 110, 110, 'IASG'),
+    getOverlayMapItem('UCOverlay', 1196, 973, 120, 120, 'UCG'),
+  ]);
+
+  return null;
+}
+
+const MapCanvasPlugin = {
+  Component: CampusBuildingOverlay,
+  connect: ['floorStore', 'setMapItems', 'linkTo'],
+};
+const id = 'campusBuildingOverlay';
+export { id, MapCanvasPlugin };
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -4,5 +4,14 @@ import * as Legend from './Legend/Legend';
 import * as NearestResult from './NearestResult/NearestResult';
 import * as ShortestResult from './ShortestResult/ShortestResult';
 import * as Pin from './Pin/Pin';
+import * as CampusBuildingOverlay from './CampusBuildingOverlay/CampusBuildingOverlay';
 
-export default [MapTile, MapItem, Pin, NearestResult, ShortestResult, Legend];
+export default [
+  MapTile,
+  CampusBuildingOverlay,
+  MapItem,
+  Pin,
+  NearestResult,
+  ShortestResult,
+  Legend,
+];
