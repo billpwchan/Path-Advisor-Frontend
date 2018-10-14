f8c61987f126b4cb601901be114666d220de407f
Henry Liu <thenrikie@hotmail.com>
2018-10-14 21:50:21 +0100

    Update default position level

--- src/reducers/appSettings.js
+++ src/reducers/appSettings.js
@@ -2,14 +2,15 @@ const initialState = {
   loading: false,
   failure: false,
   success: true,
-  levelToScale: [1.0, 0.85, 0.6, 0.5, 0.4],
+  // levelToScale: [1.0, 0.85, 0.6, 0.5, 0.4],
+  levelToScale: [1.0, 0.85, 0.6],
   meterPerPixel: 0.0455,
   minutesPerMeter: 0.015,
   defaultPosition: {
     floor: 'Overall',
     x: 936,
     y: 654,
-    level: 0,
+    level: 2,
   },
 };
 
