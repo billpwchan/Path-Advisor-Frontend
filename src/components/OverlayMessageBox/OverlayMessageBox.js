1bc45f793f5f9f95047238330d05c8b3cb95e6d0
Henry Liu <thenrikie@hotmail.com>
2018-10-21 22:07:13 +0100

    Add OverlayMessageBox Component

---
+++ src/components/OverlayMessageBox/OverlayMessageBox.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+import FullScreenOverlay from '../FullScreenOverlay/FullScreenOverlay';
+import style from './OverlayMessageBox.module.css';
+
+const OverlayMessageBox = ({ className = undefined, children, onClose }) => (
+  <FullScreenOverlay className={style.overlayBody} onClick={onClose} center>
+    <div className={classnames(style.body, className)}>{children}</div>
+  </FullScreenOverlay>
+);
+
+OverlayMessageBox.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default OverlayMessageBox;
