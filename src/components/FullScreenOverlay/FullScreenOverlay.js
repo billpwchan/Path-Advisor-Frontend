c2bbaa23f3515a069b8b002b32ca23dd72817542
Henry Liu <thenrikie@hotmail.com>
2018-10-25 15:01:44 +0100

    Update FullScreenOverlay component css

--- src/components/FullScreenOverlay/FullScreenOverlay.js
+++ src/components/FullScreenOverlay/FullScreenOverlay.js
@@ -3,11 +3,14 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import style from './FullScreenOverlay.module.css';
 
-const FullScreenOverlay = ({ className = undefined, onClick, children, center }) => (
+const FullScreenOverlay = ({ className = undefined, onClick, children, center = false }) => (
   /* eslint jsx-a11y/click-events-have-key-events: [0] */
   /* eslint jsx-a11y/no-static-element-interactions: [0] */
-  <div className={classnames(className, style.body)} onClick={onClick}>
-    {center ? <div className={style.center}>{children}</div> : { children }}
+  <div
+    className={classnames(className, style.body, { [style.bodyCenter]: center })}
+    onClick={onClick}
+  >
+    {center ? <div className={style.center}>{children}</div> : children}
   </div>
 );
 
