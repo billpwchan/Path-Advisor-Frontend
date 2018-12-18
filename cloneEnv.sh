e10049a0efdf57776dd8d65992b9bf4bd5b84f4c
Henry Liu <thenrikie@hotmail.com>
2018-12-18 11:23:02 +0000

    Add script to pull and create symlink for env and deploy script from submodule

---
+++ cloneEnv.sh
@@ -0,0 +1,7 @@
+git submodule init
+git submodule update
+find "./pathadvisor-frontend-settings" -maxdepth 1 -print | while read file; do
+  if [[ $file != *".git"* ]] && [ $file != "./pathadvisor-frontend-settings" ]; then
+    ln -s "$file" .;
+  fi
+done
