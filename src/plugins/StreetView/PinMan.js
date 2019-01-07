c4578339b8553266546e25ff57a61d49e51928a4
tzhangam@gmail.com <tzhangam@gmail.com>
2019-01-08 00:15:21 +0800

    Finished: loading small man image from local folder and turning the small man.

--- src/plugins/StreetView/PinMan.js
+++ src/plugins/StreetView/PinMan.js
@@ -1,4 +1,5 @@
-import pinManImage from './img/man/0.png';
+import PinManImages from './PinManImages';
+
 const imageWidth = 40;
 const imageHeight = 40;
 
@@ -7,17 +8,60 @@ const PIN_MAN_ID = 'PIN_MAN_ID';
 /*
     This is the pin man component after drag man is dropped on map.
     It sets an map item on the location on map where mouse is released.
+    
+    If you want to retrieve information about PinMan, for example its location, 
 */
 
 
-function PinMan({ setMapItems, removeMapItem, x, y, floor }) {
+function handleOnClick({ setMapItems, eventTrigger }) {
+    handleOnClick_dev({setMapItems,eventTrigger});
+    return;
+}
+function getPinManImage(angle) {
+    return getPinManImage_dev(angle);
+}
+
+/*
+ This is a very rudinmentary function to get pin man image given angle.
+ It basically read in the static image file loaded from PinManImages.js .
+*/
+function getPinManImage_dev(angle) {
+    const index = Math.floor(angle / 22.5);
+    const image = new Image();
+    image.src = PinManImages[index];
+    return image;
+}
+
+/*
+ This is a toy handle click function we developed to test the turning feature.
+ If you click on the small man, it will turn in clickwise direction.
+ */
+var angle = 0;
+
+async function handleOnClick_dev({ setMapItems, eventTrigger }) {
+    var id = eventTrigger.id;
+
+    console.log('clicked', eventTrigger.id);
+
+    const image = getPinManImage(angle);
+    angle = (angle + 22.5) % 360;
+    setMapItems([
+        {
+            ...eventTrigger,
+            image: image,
+        },
+    ]);
+
+}
+
+function PinMan({ setMapItems, removeMapItem, x, y, floor, angle, onClick }) {
 
     // put a read pin man in map if user specified the 'from' value in input field
     if (x && y && floor) {
         console.log('Pin Man rendered', x, y);
-        const image = new Image();
-        image.src = pinManImage;
 
+        angle = angle == null ? 0 : angle;
+        const image = getPinManImage(angle)
         setMapItems([
             {
                 id: PIN_MAN_ID,
@@ -30,8 +74,12 @@ function PinMan({ setMapItems, removeMapItem, x, y, floor }) {
                 zIndex: 2,
                 center: true,
                 scalePosition: true,
+                scaleDimension: true,
+                // onClick:(e)=>onClick(e),
+                onClick: (eventTrigger) => handleOnClick({ setMapItems, eventTrigger }),
             },
         ]);
+
     } else {
         console.log("Pin man removed");
         removeMapItem(PIN_MAN_ID);
--- src/plugins/StreetView/StreetView.js
+++ src/plugins/StreetView/StreetView.js
@@ -5,25 +5,20 @@ import DragMan from "./DragMan";
 import BaseMan from "./BaseMan";
 import PinMan from "./PinMan";
 
-const imageWidth = 40;
-const imageHeight = 40;
+/* 
+This is the highest level StreetView component. 
 
-// This is the highest level StreetView component. 
+It only communicate with the subcomponents through signals, but does not 
+touches concrete image display.
 
-// It only communicate with the subcomponents through signals, but does not 
-// touches concrete image display.
+Subcomponet interactions: 
+- Initially, set BaseMan at available mode(baseManAvail=true), and hide DragMan(displayDragMan=false).
+- Then when BaseMan is pressed, BaseMan will call handleBaseManPressed function here. StreetView then set BaseMan as unavailable(baseManAvail=false) and display DragMan(displayDragMan=true).
+- Then when DragMan is dropped, DragMan will call handleDragManDrop function here. StreetView will then hide DragMan(displayDragMan=falese) and restore BaseMan to available(baseManAvail=true).
+*/
 
-// Subcomponet interactions: 
-//* Initially, set BaseMan at available mode(baseManAvail=true), and hide DragMan(displayDragMan=false).
-//* Then when BaseMan is pressed, BaseMan will call handleBaseManPressed function here. StreetView then set BaseMan as unavailable(baseManAvail=false) and display DragMan(displayDragMan=true).
-//* Then when DragMan is dropped, DragMan will call handleDragManDrop function here. StreetView will then hide DragMan(displayDragMan=falese) and restore BaseMan to available(baseManAvail=true).
-
-// Helper function to convert mouse coordinate to canvas coordinate. The code is copied from component/MapCanvas/CanvasHandler.js
-// function getCanvasXYFromMouseXY(canvas, mouseX, mouseY) {
-//     const canvasCoordinate = canvas.getBoundingClientRect();
-//     return [mouseX - canvasCoordinate.left, mouseY - canvasCoordinate.top];
-// }
-
+const PanoRotationActivatorID = 'PIN_MAN_ID'; // This value is temporary, for development use.
+const PanoRotationListenerID = 'PANO_ROTATE_LISTENER_ID'; // This value is fixed.
 
 class StreetView extends React.Component {
     constructor(props) {
@@ -34,21 +29,26 @@ class StreetView extends React.Component {
             displayPinMan: false,
             PinManX: 0,
             PinManY: 0,
+            PinManAngle: 0,
         };
+
     }
 
-    // Helper function to convert mouse coordinate on screen to campus coordinate in map.
-    // This can be very helpful. Feel free to make good use of it.
+    /* 
+    Helper function to convert mouse coordinate on screen to campus coordinate in map.
+    The code is modified from component/MapCanvas/CanvasHandler.js.
+    This can be very helpful. Feel free to make good use of it.
+    */
     getCampusXYFromMouseXY(canvas, mouseX, mouseY) {
         // Get the zoom in/out factor for current canvas display.
-        const screenToCampusScale = {x:this.props.width/(this.props.normalizedWidth+0.0001),y:this.props.height/(this.props.normalizedHeight+0.0001)};
+        const screenToCampusScale = { x: this.props.width / (this.props.normalizedWidth + 0.0001), y: this.props.height / (this.props.normalizedHeight + 0.0001) };
 
         // Get the dimension of canvas on screen.
         const canvasCoordinate = canvas.getBoundingClientRect();
-        
+
         // Return the absolute campus coordinate where current mouse is pointing to.
-        return [(mouseX - (canvasCoordinate.left + 0.5 * canvasCoordinate.width))/screenToCampusScale.x + this.props.x,
-        (mouseY - (canvasCoordinate.top + 0.5 * canvasCoordinate.height))/screenToCampusScale.y + this.props.y];
+        return [(mouseX - (canvasCoordinate.left + 0.5 * canvasCoordinate.width)) / screenToCampusScale.x + this.props.x,
+        (mouseY - (canvasCoordinate.top + 0.5 * canvasCoordinate.height)) / screenToCampusScale.y + this.props.y];
     }
 
     handleBaseManPressed() {
@@ -58,13 +58,37 @@ class StreetView extends React.Component {
             displayDragMan: true,
             displayPinMan: false,
         });
+        // this.props.removeMapItemClickListener(PanoRotationListenerID, PanoRotationActivatorID);
     }
+
+    handlePanoRotate_dev(e) {
+        let pinManElement;
+        for (var index in this.props.mapItemStore.mapItems){
+           const item = this.props.mapItemStore.mapItems[index];
+           console.log(item.name,item.id,item.photo);
+           if (item.id === 'PIN_MAN_ID'){
+               pinManElement = item;
+               break;
+           }
+        }
+        if (pinManElement==null){
+            console.log('Pin man id not found');
+            return null;
+        }
+
+        const newAngle = this.state.PinManAngle + 22.5;
+        pinManElement.photo = "./img/man/" + (Math.floor(newAngle / 22.5)) + ".png";
+        this.setState({
+            PinManAngle: newAngle,
+        });
+    }
+    handlePanoRotate(e){
+        this.handlePanoRotate_dev(e);
+    }
+
     handleDragManDrop(e) {
-
         console.log("DragMan dropped, detected by parent.");
-
         let [x, y] = this.getCampusXYFromMouseXY(this.props.canvas, e.clientX, e.clientY);
-
         this.setState({
             baseManAvail: true,
             displayDragMan: false,
@@ -73,10 +97,9 @@ class StreetView extends React.Component {
             PinManY: y,
         });
 
-      
+        // this.props.addMapItemClickListener(PanoRotationListenerID, PanoRotationActivatorID, (e) => this.handlePanoRotate(e), false);
     }
     renderBaseMan(buttonClassName) {
-
         return (
             <BaseMan
                 available={this.state.baseManAvail}
@@ -105,32 +128,30 @@ class StreetView extends React.Component {
         if (this.state.displayPinMan) {
             x = this.state.PinManX;
             y = this.state.PinManY;
-          
             floor = this.props.floor;
         }
-     
+
         return (
             <div>
-            <PinMan
-                setMapItems={this.props.setMapItems}
-                removeMapItem={this.props.removeMapItem}
-                x={x}
-                y={y}
-                floor={floor}
-            />
+                <PinMan
+                    setMapItems={this.props.setMapItems}
+                    removeMapItem={this.props.removeMapItem}
+                    x={x}
+                    y={y}
+                    floor={floor}
+                    onClick = {(e)=>this.handlePanoRotate(e)}
+                />
             </div>
         );
     }
     render() {
         // Setting up the icon size according to platform used by client.
         console.log('Street View rendered');
-        console.log('height',this.props.height,'normHeight',this.props.normalizedHeight);
         const platform = this.props.platform;
         const buttonClassName = classnames({
             [style.buttonImage]: platform !== 'MOBILE',
             [style.buttonImageMobile]: platform === 'MOBILE',
         });
-
         return (
             <div id={id}>
                 {this.renderBaseMan(buttonClassName)}
@@ -143,7 +164,21 @@ class StreetView extends React.Component {
 
 const MapCanvasPlugin = {
     Component: StreetView,
-    connect: ['platform', 'canvas', 'setMapItems', 'removeMapItem', 'x', 'y', 'floor','height','normalizedHeight','width','normalizedWidth'],
+    connect: [
+        'platform', 
+        'canvas', 
+        'setMapItems', 
+        'removeMapItem', 
+        'x', 
+        'y', 
+        'floor', 
+        'height', 
+        'normalizedHeight', 
+        'width', 
+        'normalizedWidth', 
+        'addMapItemClickListener', 
+        'removeMapItemClickListener',
+        'mapItemStore'],
 };
 
 const id = "StreetView";
