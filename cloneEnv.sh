71af03020452621a435196eab3504b5f37a16b6f
Henry Liu <thenrikie@hotmail.com>
2019-01-20 20:44:21 +0000

    Update environment settings submodule and cloneEnv script

--- cloneEnv.sh
+++ cloneEnv.sh
@@ -2,6 +2,6 @@ git submodule init
 git submodule update
 find "./pathadvisor-frontend-settings" -maxdepth 1 -print | while read file; do
   if [[ $file != *".git"* ]] && [ $file != "./pathadvisor-frontend-settings" ]; then
-    ln -s "$file" .;
+    ln "$file" .;
   fi
 done
