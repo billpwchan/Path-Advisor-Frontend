0acf7c8c3cb3f2221f9658a65147411d5c74bd57
Henry Liu <thenrikie@hotmail.com>
2019-01-27 18:00:23 +0000

    Allow REACT_APP_API_ENDPOINT_URI to set to EMPTY keyword
    - to get around the problem https://github.com/facebook/create-react-app/issues/5833

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,15 @@
+const EMPTY = 'EMPTY';
+
 function APIEndpoint() {
-  return process.env.REACT_APP_API_ENDPOINT_URI;
+  return process.env.REACT_APP_API_ENDPOINT_URI === EMPTY
+    ? ''
+    : process.env.REACT_APP_API_ENDPOINT_URI;
 }
 
 function SearchAPIEndpoint() {
-  return process.env.REACT_APP_API_ENDPOINT_URI;
+  return process.env.REACT_APP_API_ENDPOINT_URI === EMPTY
+    ? ''
+    : process.env.REACT_APP_API_ENDPOINT_URI;
 }
 
 export { APIEndpoint, SearchAPIEndpoint };
