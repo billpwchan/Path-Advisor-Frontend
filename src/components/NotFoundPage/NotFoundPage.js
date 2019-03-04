8eaa8818564ea48c1bba3f0257904285edc1c988
Henry Liu <thenrikie@hotmail.com>
2019-03-04 12:08:01 +0000

    Update NotFoundPage message

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const NotFoundPage = () => <h1>Page not found</h1>;
+const NotFoundPage = () => (
+  <div>
+    <h1>Page not found</h1>
+    <a href="/" style={{ color: 'blue', textDecoration: 'underline' }}>
+      Click here to go back to main page
+    </a>
+  </div>
+);
 
 export default NotFoundPage;
