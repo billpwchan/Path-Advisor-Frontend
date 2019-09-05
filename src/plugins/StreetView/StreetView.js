580827023f2bfda46afbcd68b630b4cb6fa2fba8
fhfuih <sam.zyhuang@outlook.com>
2019-09-05 12:50:23 +0800

    Make orientation lazy reset on panoDisplay shown

--- src/plugins/StreetView/StreetView.js
+++ src/plugins/StreetView/StreetView.js
@@ -211,6 +211,7 @@ class StreetView extends React.Component {
         panoY: parseFloat(panoY),
         panoUrl: url,
         panoDefaultOffset: westX,
+        PinManAngle: 0,
       });
     });
   }
@@ -256,7 +257,6 @@ class StreetView extends React.Component {
       displayPinMan: false,
       displayPano: false,
       fullScreenPano: false,
-      PinManAngle: 0,
     });
     this.togglePanoItems(false);
   }
